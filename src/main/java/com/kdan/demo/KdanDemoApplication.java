f91179182d2e5393853ba15a2569ca36fc95f9cb
joel199583 <xuzhewei199583@gmail.com>
2024-11-22 21:58:22 +0800

    初始提交

---
+++ src/main/java/com/kdan/demo/KdanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kdan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdanDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdan/demo/KdanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdanDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
