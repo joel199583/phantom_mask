b15bec6afa96c4fff9872f84b736d660de8f908a
JoelXu <xuzhewei199583@gmail.com>
2024-11-26 17:45:38 +0800

    update KdanDemoApplication

--- src/main/java/com/kdan/demo/KdanDemoApplication.java
+++ src/main/java/com/kdan/demo/KdanDemoApplication.java
@@ -1,15 +1,24 @@
 package com.kdan.demo;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.kdan.demo.mapper")
+@RestController
 public class KdanDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KdanDemoApplication.class, args);
 	}
+	
+	@RequestMapping(value = {"/", ""})
+    public String Hello() {
+    	return "<h1>Welcome<h1>";
+    }
 
 }
